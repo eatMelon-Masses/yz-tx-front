81d24c3710723ee1d83dd9c2768120603cbfef93
eatMelon-Masses <z11764404@gmail.com>
2022-09-24 23:13:48 +0800

    初始化

---
+++ .umirc.ts
@@ -0,0 +1,44 @@
+import { defineConfig } from '@umijs/max';
+export default defineConfig({
+  proxy:{
+    
+      '/api': {
+        'target': 'http://hk.yiezi.ml:32247/',
+        'changeOrigin': true,
+        'pathRewrite': { '^/api' : '/' },
+        // headers:{'host': '127.0.0.1'}
+      },
+    
+  },
+  antd: {},
+  access: {},
+  model: {},
+  initialState: {},
+  request: {},
+  layout: {
+    title: '@umijs/max',
+  },
+  routes: [
+    {
+      path: '/',
+      redirect: '/home',
+    },
+    {
+      name: '首页',
+      path: '/home',
+      component: './Home',
+    },
+    {
+      name: '权限演示',
+      path: '/access',
+      component: './Access',
+    },
+    {
+        name: ' CRUD 示例',
+        path: '/table',
+        component: './Table',
+    },
+  ],
+  npmClient: 'yarn',
+});
+
